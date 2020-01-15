1e4ef0fc5074bff223d3c81930812b228186d087
Eugeny <info@coders.studio>
2020-01-16 00:52:35 +0300

    added posts table

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::pattern('post', '[0-9]+');
+Route::resource('admin/posts', 'Admin\PostController');
+Route::post('admin/posts/massdestroy', 'Admin\PostController@massDestroy')->name('posts.massdestroy');
+Route::put('admin/posts/{post}/toggleboolean', 'Admin\PostController@toggleBoolean')->name('posts.toggleboolean');
